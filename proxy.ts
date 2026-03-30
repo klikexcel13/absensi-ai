c6ee97da3f3bedf2bee0d94edbe599f28bbdadb8
klikexcel13 <klik.excel13@gmail.com>
2026-03-30 15:14:02 +0700

    fix: proxy jalan mulus 100 persen

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,12 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware"; 
 
-// Fungsi ini akan dieksekusi Next.js sebelum halaman apapun dimuat
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export async function proxy(request: NextRequest) {
+  return await updateSession(request);
 }
 
-// Menentukan halaman mana saja yang dijaga oleh Satpam
 export const config = {
   matcher: [
-    /*
-     * Match semua request paths KECUALI untuk:
-     * - _next/static (file statis Next.js)
-     * - _next/image (optimasi gambar Next.js)
-     * - favicon.ico (ikon website)
-     * - file gambar (svg, png, jpg, dll)
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-}
+};
